4ed686e1efac50ed5dab2839edeb4f9f128c1689
Laurence Elsdon <git@elsdon.me>
2015-07-10 15:03:11 +0100

    Data.isset()

--- app/factories/data.js
+++ app/factories/data.js
@@ -4,56 +4,49 @@ angular.module('app.dataFactory', [])
 
 /*****************************************************************
 *
-* User factory
+* Data factory
 *
 ******************************************************************/
 .factory('Data', function(localStorageService, Config) {
-  var Data = {
+  return {
 
     /**
-     * User token getter
+     * Data getter
      *
-     * @return {String} token
+     * @return {Array} data
      */
     get: function() {
       return localStorageService.get('data')
     },
 
     /**
-     * User token setter
+     * Data setter
      *
-     * @return {String} token
+     * @return {Array} data
      */
     set: function(data) {
       return localStorageService.set('data', data)
     },
 
     /**
-     * User token unset
+     * Data is set
+     *
+     * @return {Bool} data
+     */
+    isset: function() {
+      if(localStorageService.get('data')) {
+        return true
+      }
+      else {
+        return false
+      }
+    },
+
+    /**
+     * Data unset
      */
     unset: function() {
-      console.log('Data Unset')
       return localStorageService.remove('data')
     }
   }
-
-  /**
-   * Init 
-   *
-   * Check whether a JSON Web Token is present in local storage
-   * if it is then fetch the user data from the server
-   * otherwise just check the server is online
-   *
-   * @return {Object} user 
-   */
-  // var init = function() {
-  //   // if(User.token.get()) {
-  //   //   User.fetch()
-  //   // } 
-  //   // else {
-  //   //   User.fetch('guest')
-  //   // }
-  //   return Data
-  // }
-  return Data
 });

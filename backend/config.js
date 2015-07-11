e710cc0654980c26e4e943a2e87042659c94147f
Laurence Elsdon <git@elsdon.me>
2015-07-11 18:19:48 +0100

    Fixed port

--- backend/config.js
+++ backend/config.js
@@ -7,7 +7,7 @@ module.exports = {
   server_port: 8552,
 
   // App url
-  app_url: 'http://localhost:8552',
+  app_url: 'http://localhost:8551',
 
   // Toshl client_id
   client_id: '52ecf9d5-9f52-451d-b499-e916c27215e7ddada463b55bfe88ae0ffee8b7eaca42'
